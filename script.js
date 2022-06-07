cbe33daa4f953bbd106a94ee975de879e43ff4ef
Katheren Salom <katherensalom@gmail.com>
2022-06-07 18:18:20 +0000

    creando codigo en html y se creo archivo script para el js

---
+++ script.js
@@ -0,0 +1,15 @@
+function GenerarExcusasAleatorio(quien, que, cuando) {
+  /*let who = ['the dog','my granma','his turtle','my bird'];
+    let what = ['eat','pissed','crushed','broked'];
+    let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];*/
+
+  let who = ["el perro", "mi abuela", "su tortuga", "mi pájaro"];
+  let what = ["comer", "molesta (o)", "aplastada (o)", "roto"];
+  let when = ["antes de la clase","justo a tiempo","cuando terminé","durante mi almuerzo","mientras estaba orando"];
+  
+}
+
+function load() {
+  alert("evento load detectado!");
+}
+window.onload = load;
